4a4644c693c8367d21299d657e01134798f4a5aa
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-05-12 12:22:41 +0100

    model and theme colors added

---
+++ lib/app/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFF0F172A);
+  static const card = Color(0xFF1E293B);
+  static const primary = Color(0xFF3B82F6);
+  static const userBubble = Color(0xFF2563EB);
+  static const botBubble = Color(0xFF1E293B);
+  static const textWhite = Colors.white;
+}
